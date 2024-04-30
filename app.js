c7b3c6ccf2606c123e1eac4b3ba3fbe27108711d
Aman Saeed <amantatla312@gmail.com>
2024-04-30 10:18:59 +0500

    check

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(cors({
   origin: 'http://localhost:4200'
 }));
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
